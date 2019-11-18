31f3f8358e54774584d79ca32e277e98b622c5b7
Ramesh Kumar <codeofnode@gmail.com>
2019-11-18 11:45:42 +0530

    MASTER- todo script ready

--- TODO.md
+++ TODO.md
@@ -1,10 +1,2 @@
 # TODO
 All todos will be noted in this file, with ref to the issue id
-
-## [0.0.0]  - 2020-01-01
-### To Be Added
-- [0.0.0-2] Working POC of zero effort testing with allrounder
-
-
-  [0.0.0]: https://github.com/codeofnode/todo/tree/v0.0.0
-  [0.0.0-2]: https://github.com/codeofnode/todo/issues/2
--- scripts/todo.js
+++ scripts/todo.js
@@ -16,41 +16,34 @@ const octokit = Octokit({
 let title = process.argv[3]
 let label = process.argv[2]
 const argvLength = process.argv.length
-if (process.argv[argvLength - 1].split('.').length !== 3) {
-  title = process.argv.slice(1).join(' ')
+if (process.argv[argvLength - 1].split('.').length !== 3 && process.argv[4].split(' ').length > 1) {
+  title = process.argv.slice(3).join(' ')
   process.argv[4] = 0
 }
 
 switch (label) {
   case 'a': label = 'Added'; break
-  case 'b': label = 'Bug'; break
+  case 'f': label = 'Fixed'; break
   case 'c': label = 'Changed'; break
-  case 'r': label = 'Removed'; break
+  case 'd': label = 'Removed'; break
   default :
     console.log('Invalid type')
     process.exit(1)
 }
 
-const createIssue = function () {
-  console.log({
+const createIssue = function (currRelease) {
+  return process.argv[5] || octokit.issues.create({
     owner,
     repo: pkg.name,
-    title: label,
-    labels: [label]
+    title: title,
+    labels: ['ToBe' + label, currRelease]
   })
-  return 101
-  // return octokit.issues.create({
-  //  owner,
-  //  repo: pkg.name,
-  //  title: label,
-  //  labels: [label]
-  // })
 }
 
 const getRelease = async function () {
   if (process.argv[4]) return process.argv[4]
   const release = (await execute('git symbolic-ref --short HEAD')).stdout
-  const sps = release.split('-').pop().split('.')
+  const sps = release.split('-').shift().split('.')
   if (sps.length !== 3) {
     console.log('Release not found from branch ' + release)
     process.exit(1)
@@ -60,6 +53,7 @@ const getRelease = async function () {
 
 const appendIntoTODO = async function (release, issueNumber) {
   let currentTODO = fs.readFileSync('TODO.md').toString()
+  if (currentTODO.indexOf('- [' + release + '-' + issueNumber + ']') !== -1) return
   if (currentTODO.indexOf('\n\n\n') === -1) {
     currentTODO += '\n\n\n'
   }
@@ -69,44 +63,57 @@ const appendIntoTODO = async function (release, issueNumber) {
   const ln = sps.length
   linkSps.push('  [' + release + '-' + issueNumber + ']: ' + pkg.homepage + '/issues/' + issueNumber)
   if (currentTODO.indexOf('## [' + release + ']') === -1) {
-    sps.splice(3, 0, '', '',
+    sps.splice(2, 0, '',
       '## [' + release + ']',
-      '### ' + label,
+      '### To Be ' + label,
       '- [' + release + '-' + issueNumber + '] ' + title
     )
     linkSps.push('  [' + release + ']: ' + pkg.homepage + '/tree/' + release)
   } else {
     let k, j
+    let noL = false
     for (k = 0; k < ln; k++) {
       if (sps[k].indexOf('## [' + release + ']') === 0) {
         break
       }
     }
     for (j = k; j < ln; j++) {
-      if (sps[j].indexOf('### ' + label) === 0) {
+      if (sps[j] === '') {
+        noL = true
+        break
+      }
+      if (sps[j].indexOf('### To Be ' + label) === 0) {
         break
       }
     }
-    if (j >= ln) {
-      sps.splice(j, 0, '',
-        '### ' + label,
-        '- [' + release + '-' + issueNumber + '] ' + title)
+    if (noL || j >= ln) {
+      sps.splice(k + 1, 0,
+        '### To Be ' + label,
+        '- [' + release + '-' + issueNumber + '] ' + title
+      )
     } else {
-      sps.splice(j, 0,
+      sps.splice(j + 1, 0,
         '- [' + release + '-' + issueNumber + '] ' + title)
     }
   }
   await Promise.all([
-    writeFile('TODO.md', sps.join('\n') + '\n\n\n' + linkSps.sort().join('\n'))
+    writeFile(
+      'TODO.md',
+      sps.join('\n') +
+        '\n\n\n' +
+        linkSps.sort((a, b) =>
+          a.substr(0, a[9] === '-' ? 20 : 9) < b.substr(0, b[9] === '-' ? 20 : 9)
+            ? -1
+            : 1
+        ).join('\n')
+    )
   ])
 }
 
 async function main () {
   const currRelease = await getRelease()
-  await execute('git checkout ' + currRelease)
-  const issueNumber = await createIssue()
-  await execute('git checkout -b ' + currRelease + '-' + issueNumber)
-  // await execute('git push origin ' + currRelease + '-' + issueNumber + ':' + currRelease + '-' + issueNumber)
+  const issueNumber = await createIssue(currRelease)
+  await execute('git checkout -b ' + currRelease + '-' + issueNumber + ' ' + currRelease)
   return appendIntoTODO(currRelease, issueNumber)
 }
 
